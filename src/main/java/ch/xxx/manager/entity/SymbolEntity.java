e948d315ca8d95d497d903fb2fc28d47fe7a5e79
Angular2Guy <angular2guy@gmail.com>
2019-11-17 15:48:11 +0100

    make symbol import work

---
+++ src/main/java/ch/xxx/manager/entity/SymbolEntity.java
@@ -0,0 +1,53 @@
+/**
+ *    Copyright 2019 Sven Loesekann
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package ch.xxx.manager.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("symbol")
+public class SymbolEntity {
+	@Id
+	private Long id;
+	private String symbol;
+	private String name;
+	
+	public SymbolEntity() {		
+	}
+	
+	public SymbolEntity(Long id, String symbol, String name) {
+		super();
+		this.id = id;
+		this.symbol = symbol;
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
--- src/main/resources/local/schema.sql
+++ src/main/resources/local/schema.sql
@@ -1,3 +1,4 @@
 create sequence mainseq start with 1000 increment by 100;
 create table appuser (id bigint identity primary key, name varchar(50), firstname varchar(50), birthdate date);
 create table dailyquote (id bigint identity primary key,symbol varchar(15), open decimal(12,4), high decimal(12,4), low decimal(12,4), close decimal(12,4), volume bigint, day date);
+create table symbol (id bigint identity primary key, symbol varchar(15), name varchar(100));
