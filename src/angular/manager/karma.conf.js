cd18810544eb93dc7134e6695e5480e317e67b94
Angular2Guy <angular2guy@gmail.com>
2020-02-24 21:57:59 +0100

    Angular 9 update

--- src/angular/manager/karma.conf.js
+++ src/angular/manager/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter') 
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,7 +26,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chromium', 'ChromeHeadless', 'ChromiumHeadless'],
+    customLaunchers: {
+        ChromeHeadless: {
+          base: 'Chrome',
+          flags: ['--no-sandbox','--headless', '--disable-gpu', '--remote-debugging-port=9222']
+        },
+        ChromiumHeadless: {
+            base: 'Chromium',
+            flags: ['--no-sandbox','--headless', '--disable-gpu', '--remote-debugging-port=9222']
+          }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
