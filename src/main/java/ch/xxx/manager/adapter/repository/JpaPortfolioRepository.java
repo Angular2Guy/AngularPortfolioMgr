b1ff19e8f5e037f88da1ae2928aef00d5dc202e0
Angular2Guy <angular2guy@gmail.com>
2021-07-14 13:41:18 +0200

    fix

--- src/main/java/ch/xxx/manager/adapter/repository/JpaPortfolioRepository.java
+++ src/main/java/ch/xxx/manager/adapter/repository/JpaPortfolioRepository.java
@@ -23,7 +23,7 @@ import ch.xxx.manager.domain.model.entity.dto.PortfolioAndSymbolDto;
 public interface JpaPortfolioRepository extends JpaRepository<Portfolio, Long> {
 	@Query("select p from Portfolio p where p.appUser.id = :userId")
 	List<Portfolio> findByUserId(Long userId);
-	@Query("select new ch.xxx.manager.entity.dto.PortfolioAndSymbolDto(p.id, au.id, p.name, p.createdAt, pts.weight, pts.changedAt, "
+	@Query("select new ch.xxx.manager.domain.model.entity.dto.PortfolioAndSymbolDto(p.id, au.id, p.name, p.createdAt, pts.weight, pts.changedAt, "
 			+ "pts.removedAt, s.id, s.symbol, s.name, s.currencyKey) "
 			+ "from Portfolio p inner join p.appUser au inner join p.portfolioToSymbols pts inner join pts.symbol s "
 			+ "where p.id = :portfolioId")
