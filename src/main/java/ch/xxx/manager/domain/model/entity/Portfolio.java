f1deac394979a8748b3cc73ab80ab5e4ee508825
Angular2Guy <angular2guy@gmail.com>
2021-07-19 08:57:23 +0200

    fix

--- src/main/java/ch/xxx/manager/domain/model/entity/Portfolio.java
+++ src/main/java/ch/xxx/manager/domain/model/entity/Portfolio.java
@@ -17,12 +17,16 @@ import java.time.LocalDate;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+
+import ch.xxx.manager.domain.utils.CurrencyKey;
 
 
 @Entity
@@ -33,6 +37,8 @@ public class Portfolio {
 	@ManyToOne
 	private AppUser appUser;
 	private String name;
+	@Enumerated(EnumType.STRING)
+	private CurrencyKey currencyKey;
 	private LocalDate createdAt;
 	private BigDecimal month1;
 	private BigDecimal month6;
