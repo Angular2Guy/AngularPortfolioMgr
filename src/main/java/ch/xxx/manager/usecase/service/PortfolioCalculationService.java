607d9300312d895b5548cd17acd547f6d3b31741
Angular2Guy <angular2guy@gmail.com>
2021-08-19 17:10:39 +0200

    fixes

--- src/main/java/ch/xxx/manager/usecase/service/PortfolioCalculationService.java
+++ src/main/java/ch/xxx/manager/usecase/service/PortfolioCalculationService.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
