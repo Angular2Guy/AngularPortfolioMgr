a4548e8f9b055a183792fb08e5fa1443f6db3154
Angular2Guy <angular2guy@gmail.com>
2021-11-13 15:00:36 +0100

    java 17 update

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#./gradlew build -PwithAngular=true -PuseChromium=true
+./gradlew build -PwithAngular=true
 docker build -t angular2guy/angularportfoliomgr:latest --build-arg JAR_FILE=build/libs/portfoliomgr.jar --no-cache .
 docker run -p 8080:8080 --network="host" angular2guy/angularportfoliomgr:latest
