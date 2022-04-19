ee01a106844dfddde2993915ee50e2c920802d74
Sven Loesekann <angular2guy@gmail.com>
2022-04-19 18:16:21 +0200

    delete dir

--- backend/build/resources/main/local/data.sql
+++
@@ -1,3 +0,0 @@
-insert into appuser (username, birthdate, updated_at, password, email_address, user_role, locked, enabled, uuid) 
-	values ('max', now(), now(), 'pwd', 'sven@gmx.de', 'USERS', false, true, 'uuid');
---insert into dailyquote (symbol, open, high, low, close, volume, day) values ('MSFT',92.7500,94.5000,91.9380,29.8767,46349000,now());
