13afe75a413855a8f4f0eb64fec23d146892efed
Angular2Guy <angular2guy@gmail.com>
2022-04-24 20:12:36 +0200

    fix: Dlt

--- backend/src/main/java/ch/xxx/manager/adapter/consumer/KafkaConsumer.java
+++ backend/src/main/java/ch/xxx/manager/adapter/consumer/KafkaConsumer.java
@@ -17,7 +17,13 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,15 +36,23 @@ import ch.xxx.manager.domain.model.dto.RevokedTokenDto;
 
 @Service
 @Transactional
-@Profile({"kafka","prod-kafka"})
+@Profile({ "kafka", "prod-kafka" })
 public class KafkaConsumer {
 	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
-	
+
+	@RetryableTopic(attempts = "3", backoff = @Backoff(delay = 1000, multiplier = 2.0), autoCreateTopics = "false", topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
 	@KafkaListener(topics = KafkaConfig.NEW_USER_TOPIC)
 	public void consumerForNewUserTopic(String message) throws JsonMappingException, JsonProcessingException {
 		LOGGER.info("consumberForNewUserTopic [{}]", message);
 		new ObjectMapper().readValue(message, AppUserDto.class);
 	}
+
+	@DltHandler
+	public void dlt(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+		LOGGER.info(in + " from " + topic);
+	}
+
+	@RetryableTopic(attempts = "3", backoff = @Backoff(delay = 1000, multiplier = 2.0), autoCreateTopics = "false", topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
 	@KafkaListener(topics = KafkaConfig.USER_LOGOUT_TOPIC)
 	public void consumerForUserLogoutsTopic(String message) throws JsonMappingException, JsonProcessingException {
 		LOGGER.info("consumerForUserLogoutsTopic [{}]", message);
