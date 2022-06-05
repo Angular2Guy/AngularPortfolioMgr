8610b48e3ee3a2635ed72c2fda28f549d161b842
Angular2Guy <angular2guy@gmail.com>
2022-06-05 17:20:43 +0200

    refactor: forms

--- frontend/src/angular/src/app/portfolios/components/new-portfolio/new-portfolio.component.ts
+++ frontend/src/angular/src/app/portfolios/components/new-portfolio/new-portfolio.component.ts
@@ -11,7 +11,7 @@
    limitations under the License.
  */
 import { Component, OnInit, Inject } from '@angular/core';
-import { UntypedFormGroup, Validators, UntypedFormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { OverviewComponent } from '../overview/overview.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Portfolio } from '../../../model/portfolio';
@@ -19,32 +19,38 @@ import { PortfolioData } from '../../../model/portfolio-data';
 import { TokenService } from '../../../service/token.service';
 import { DateTime } from 'luxon';
 
+enum FormFields {
+	PortfolioName = 'portfolioName',
+	CreatedAt = 'createdAt'
+}
+
 @Component({
   selector: 'app-new-portfolio',
   templateUrl: './new-portfolio.component.html',
   styleUrls: ['./new-portfolio.component.scss']
 })
 export class NewPortfolioComponent {
-  portfolioForm: UntypedFormGroup;
+  portfolioForm: FormGroup;
   formValid = true;
+  FormFields = FormFields;
 
   constructor(public dialogRef: MatDialogRef<OverviewComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: PortfolioData,
 		private tokenService: TokenService,
-		private fb: UntypedFormBuilder) { 
+		private fb: FormBuilder) { 
 		this.portfolioForm = this.fb.group({
-			portfolioName: ['', Validators.required],
-			createdAt: [DateTime.fromISO(this.data.portfolio.createdAt), Validators.required]
+			[FormFields.PortfolioName]: ['', Validators.required],
+			[FormFields.CreatedAt]: [DateTime.fromISO(this.data.portfolio.createdAt), Validators.required]
 		}, {
 			validator: this.validate.bind(this)
 		});
   }
 
   onAddClick(): void {
-	const createdAt = this.portfolioForm.get('createdAt').value as DateTime;
+	const createdAt = this.portfolioForm.get(FormFields.CreatedAt).value as DateTime;
 	//createdAt.setMinutes(createdAt.getMinutes() - createdAt.getTimezoneOffset());
 	const portfolio: Portfolio = {id: null,createdAt: new Date(createdAt.toMillis()).toISOString(),
-		 month1: null, month6: null, name: this.portfolioForm.get('portfolioName').value, 
+		 month1: null, month6: null, name: this.portfolioForm.get(FormFields.PortfolioName).value, 
 		symbols: [], userId: this.tokenService.userId, year1: null, year10: null, year2: null, year5: null }; 		
 	this.dialogRef.close(portfolio);		
   }
@@ -53,14 +59,14 @@ export class NewPortfolioComponent {
 	this.dialogRef.close();
   }
 
-  validate(formGroup: UntypedFormGroup) {
-	if (formGroup.get('portfolioName').touched) {
-		const myValue: string = formGroup.get('portfolioName').value;
+  validate(formGroup: FormGroup) {
+	if (formGroup.get(FormFields.PortfolioName).touched) {
+		const myValue: string = formGroup.get(FormFields.PortfolioName).value;
 		if(myValue && myValue.trim().length > 4) {
-			formGroup.get('portfolioName').setErrors(null);
+			formGroup.get(FormFields.PortfolioName).setErrors(null);
 			this.formValid = true;
 		} else {
-			formGroup.get('portfolioName').setErrors({ MatchPassword: true });
+			formGroup.get(FormFields.PortfolioName).setErrors({ MatchPassword: true });
 			this.formValid = false;			
 		}
 	}
