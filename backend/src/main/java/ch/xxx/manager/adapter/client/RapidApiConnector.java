7213a107441f7ff9b465515853ece83bf56f2d39
Angular2Guy <angular2guy@gmail.com>
2022-09-02 11:35:40 +0200

    feat: profile

---
+++ backend/src/main/java/ch/xxx/manager/adapter/client/RapidApiConnector.java
@@ -0,0 +1,59 @@
+/**
+ *    Copyright 2019 Sven Loesekann
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package ch.xxx.manager.adapter.client;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import ch.xxx.manager.usecase.service.RapidApiClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RapidApiConnector implements RapidApiClient {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RapidApiConnector.class);
+	@Value("${api.key.rapidapi}")
+	private String apiKey;
+	@Value("${show.api.key}")
+	private String showApiKey;
+
+	@PostConstruct
+	public void init() {
+		if ("true".equalsIgnoreCase(this.showApiKey)) {
+			LOGGER.info("RapidApiKey: " + apiKey);
+		}
+	}
+
+	@Override
+	public Mono<String> importCompanyProfile(String symbol) {
+		try {
+			final String myUrl = String.format("https://yh-finance.p.rapidapi.com/stock/v2/get-profile?symbol=%s", symbol);
+			LOGGER.info(myUrl);
+			return WebClient.create().mutate().exchangeStrategies(ConnectorUtils.createLargeResponseStrategy()).build()
+					.get().uri(new URI(myUrl))
+					.header("X-RapidAPI-Key", this.apiKey)
+					.header("X-RapidAPI-Host", "yh-finance.p.rapidapi.com")
+					.retrieve().bodyToMono(String.class);
+		} catch (URISyntaxException e) {
+			LOGGER.info("importCompanyProfile failed.", e);
+		}
+		return Mono.empty();
+	}
+}
--- backend/src/main/java/ch/xxx/manager/domain/model/entity/Symbol.java
+++ backend/src/main/java/ch/xxx/manager/domain/model/entity/Symbol.java
@@ -19,11 +19,7 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 
 import ch.xxx.manager.domain.utils.CurrencyKey;
 
