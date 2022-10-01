c8b6af2677273c5f6daa688952d99b816a0fd198
Angular2Guy <angular2guy@gmail.com>
2022-10-01 12:43:49 +0200

    docs: update

--- docker-postgres.sh
+++ docker-postgres.sh
@@ -1,5 +1,5 @@
-docker pull postgres:13
-docker run --name local-postgres-pfm -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=portfoliomgr -p 5432:5432 -d postgres
+docker pull postgres:12
+docker run --name local-postgres-pfm -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=portfoliomgr -p 5432:5432 -d postgres:12
 
 # docker start local-postgres-pfm
 # docker stop local-postgres-pfm
