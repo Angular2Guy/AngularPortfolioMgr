0a2d155657f48a21d4495dce3d10f2ce8cb72a9c
angular2guy <angular2guy@gmail.com>
2023-01-20 09:52:54 +0100

    fix: improve

--- backend/src/test/java/ch/xxx/manager/adapter/repository/AppUserRepositoryTest.java
+++ backend/src/test/java/ch/xxx/manager/adapter/repository/AppUserRepositoryTest.java
@@ -19,13 +19,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import ch.xxx.manager.domain.model.entity.AppUser;
 import ch.xxx.manager.domain.model.entity.AppUserRepository;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AppUserRepositoryTest {
 
 	@Autowired
