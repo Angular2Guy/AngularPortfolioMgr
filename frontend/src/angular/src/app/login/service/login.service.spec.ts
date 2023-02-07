b2dcca40e6f3d13c29b144dcff6cf19b0420afb7
Angular2guy <angular2guy@gmail.com>
2023-02-07 18:06:07 +0100

    fix: add test

--- frontend/src/angular/src/app/login/service/login.service.spec.ts
+++ frontend/src/angular/src/app/login/service/login.service.spec.ts
@@ -10,17 +10,48 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { TestBed } from "@angular/core/testing";
-
+import { Observable, of } from "rxjs";
+import { Login } from "../model/login";
 import { LoginService } from "./login.service";
 
-/*
-describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+interface Obs {
+	pipe(fn: (x,y) => void): Observable<boolean>;
+}
 
-  it('should be created', () => {
-    const service: LoginService = TestBed.get(LoginService);
-    expect(service).toBeTruthy();
+describe('LoginService', () => {
+  const login = {
+		emailAddress: 'email@email.com',
+		password: 'password',
+		username: 'username',
+		token: 'token'
+  } as Login;
+  let service: LoginService = null;
+  let obsSpy;
+  beforeEach(() => {
+	  const httpSpy = jasmine.createSpyObj('http', ['post']);
+      obsSpy = jasmine.createSpyObj('obs', ['pipe']);		  
+	  httpSpy.post.and.returnValue(obsSpy);
+	  service = new LoginService(httpSpy,  
+	  	jasmine.createSpyObj('tokenService', ['createTokenHeader'],{'secUntilNextLogin': 60}));	  	  
+	  });
+
+  it('should post signin', () => {	 
+	obsSpy.pipe.and.returnValue(of(true));
+    const result = service.postSignin(login);
+    expect(result).toBeTruthy();
+    result.subscribe(value => {
+		expect(value).toBe(true);
+	});
   });
-});
-*/
+  
+  it('should post login', () => {
+	  obsSpy.pipe.and.returnValue(of(login));
+		const result = service.postLogin(login);
+		expect(result).toBeTruthy();	
+		result.subscribe(value => {
+			expect(value.username).toBe(login.username);
+			expect(value.password).toBe(login.password);
+			});		  
+	  });
+  });
+
