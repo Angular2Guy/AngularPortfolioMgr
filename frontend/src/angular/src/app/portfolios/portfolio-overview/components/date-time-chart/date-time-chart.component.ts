3bf6ad77a8aa0776ef2beacf8dd78e6cafc17b31
Angular2guy <angular2guy@gmail.com>
2023-06-02 15:21:39 +0200

    refactor: header

--- frontend/src/angular/src/app/portfolios/portfolio-overview/components/date-time-chart/date-time-chart.component.ts
+++ frontend/src/angular/src/app/portfolios/portfolio-overview/components/date-time-chart/date-time-chart.component.ts
@@ -22,45 +22,35 @@ import {
 } from "@angular/core";
 import { DateTime, Interval } from "luxon";
 import { Item } from "../../model/item";
-import { CalendarService } from "../../service/calendar.service";
+import { DateTimeChartBase } from "./date-time-chart-base";
 
 @Component({
   selector: "app-date-time-chart",
   templateUrl: "./date-time-chart.component.html",
   styleUrls: ["./date-time-chart.component.scss"],
 })
-export class DateTimeChartComponent implements OnInit, AfterViewInit {
-  private localItems: Item<Event>[] = [];
-  private localStart: Date = new Date();
-  private localShowDays: boolean;
-  protected end: Date;
+export class DateTimeChartComponent extends DateTimeChartBase implements OnInit, AfterViewInit {  
   protected dayPx = -10;
-  protected periodDays: DateTime[] = [];
-  protected periodMonths: DateTime[] = [];
-  protected periodYears: DateTime[] = [];
-  protected monthHeaderAnchorIds: string[] = [];
-  protected yearHeaderAnchorIds: string[] = [];
   protected anchoreIdIndex = 0;
   protected nextAnchorId = "";
   protected timeChartHeight = 0;
-  protected readonly DAY_WIDTH = CalendarService.DAY_WIDTH;
-  protected readonly MONTH_WIDTH = CalendarService.MONTH_WIDTH;
   protected readonly CURRENT_TIME = "currentTime";
 
   @ViewChild("timeChart")
   private timeChartRef: ElementRef;
 
-  constructor(
-    protected calendarService: CalendarService,
-    @Inject(LOCALE_ID) private locale: string
-  ) {}
+  constructor(    
+    @Inject(LOCALE_ID) locale: string
+  ) {
+	  super(locale);
+  }
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.timeChartHeight = this.timeChartRef.nativeElement.offsetHeight;      
     });
     setTimeout(() => {
-		console.log('afterViewInit');
+		//console.log('afterViewInit');
 		let myPeriods = !this.showDays ? this.periodYears : this.periodMonths;
 		myPeriods = myPeriods.filter(myPeriod => myPeriod.diffNow().seconds <= 0);
 		const myPeriodIndex = myPeriods.length === 0 ? -1 : myPeriods.length-1;
@@ -169,17 +159,6 @@ export class DateTimeChartComponent implements OnInit, AfterViewInit {
     return result;
   }
 
-  protected generateHeaderAnchorId(dateTime: DateTime): string {
-    const headerAnchorId =
-      "" +
-      dateTime.year +
-      "_" +
-      dateTime.month +
-      "_" +
-      new Date().getMilliseconds().toString(16);
-    return headerAnchorId;
-  }
-
   protected scrollToTime(timeDiff: number): void {
     const anchorIds = !this.showDays
       ? this.yearHeaderAnchorIds
@@ -200,76 +179,6 @@ export class DateTimeChartComponent implements OnInit, AfterViewInit {
       behavior: "smooth",
       inline: "nearest",
     });
-  }
-
-  private calcChartTime(): void {
-    const myEndOfYear = new Date(new Date().getFullYear(), 11, 31, 23, 59, 59);
-    const endOfYear = DateTime.fromJSDate(myEndOfYear)
-      .setLocale(this.locale)
-      .setZone(Intl.DateTimeFormat().resolvedOptions().timeZone)
-      .toJSDate();
-    let myItem = new Item<Event>();
-    myItem.end = DateTime.fromJSDate(new Date(0, 11, 31))
-      .setLocale(this.locale)
-      .setZone(Intl.DateTimeFormat().resolvedOptions().timeZone)
-      .toJSDate();
-    const lastEndItem = this.items.reduce(
-      (acc, newItem) =>
-        acc?.end?.getMilliseconds() < newItem?.end?.getMilliseconds()
-          ? newItem
-          : acc,
-      myItem
-    );
-    const openEndItems = this.items.filter((newItem) => !newItem?.end);
-    this.end =
-      openEndItems.length > 0 || !this.showDays
-        ? endOfYear
-        : lastEndItem.end.getFullYear() < 1
-        ? endOfYear
-        : lastEndItem.end;
-    this.periodDays = [];
-    for (
-      let myDay = DateTime.fromObject({
-        year: this.start.getFullYear(),
-        month: this.start.getMonth() + 1,
-        day: 1,
-      });
-      myDay.toMillis() <= DateTime.fromJSDate(this.end).toMillis();
-      myDay = myDay.plus({ days: 1 })
-    ) {
-      this.periodDays.push(myDay);
-    }
-    this.periodMonths = [];
-    this.monthHeaderAnchorIds = [];
-    for (
-      let myMonth = DateTime.fromObject({
-        year: this.start.getFullYear(),
-        month: !!this.showDays ? this.start.getMonth() + 1 : 1,
-        day: 1,
-      });
-      myMonth.toMillis() <= DateTime.fromJSDate(this.end).toMillis();
-      myMonth = myMonth.plus({ months: 1 })
-    ) {
-      this.periodMonths.push(myMonth);
-      this.monthHeaderAnchorIds.push(
-        "M_" + this.generateHeaderAnchorId(myMonth)
-      );
-    }
-    this.periodYears = [];
-    this.yearHeaderAnchorIds = [];
-    for (
-      let myYear = DateTime.fromObject({
-        year: this.start.getFullYear(),
-        month: 1,
-        day: 1,
-      });
-      myYear.toMillis() <= DateTime.fromJSDate(this.end).toMillis();
-      myYear = myYear.plus({ years: 1 })
-    ) {
-      this.periodYears.push(myYear);
-      this.yearHeaderAnchorIds.push("Y_" + this.generateHeaderAnchorId(myYear));
-    }
-    console.log('onInit');
   }
 
   get items(): Item<Event>[] {
