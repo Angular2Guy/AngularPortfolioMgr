dc8e4521f2d8dda0cacc7741e700bd1d99467d82
Angular2guy <angular2guy@gmail.com>
2025-05-21 22:37:33 +0200

    fix: request

--- backend/src/main/java/ch/xxx/manager/adapter/client/NewsFeedConnector.java
+++ backend/src/main/java/ch/xxx/manager/adapter/client/NewsFeedConnector.java
@@ -41,6 +41,7 @@ public class NewsFeedConnector implements NewsFeedClient {
     private static final String SEEKING_ALPHA_URL = "https://seekingalpha.com/market_currents.xml";
     private static final String CNBC_FINANCE_URL = "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664";
     private static final String SEC_EDGAR_USGAAP = "https://www.sec.gov/Archives/edgar/usgaap.rss.xml";
+    private static final String SEC_EDGAR_ICON = "https://www.sec.gov/favicon.icon";
     private static final Logger LOGGER = LoggerFactory.getLogger(NewsFeedConnector.class);
     private final RestClient restClient;
     private final XmlMapper xmlMapper;
@@ -64,7 +65,11 @@ public class NewsFeedConnector implements NewsFeedClient {
 
     @Override
     public List<CompanyReportWrapper> importSecEdgarUsGaapNewsFeed() {
-        var result = this.loadFile(SEC_EDGAR_USGAAP, String.class);
+        //var result = this.loadFile(SEC_EDGAR_USGAAP, String.class);
+        record ResultItem(String content, Boolean icon) {}
+        var result = List.of(SEC_EDGAR_USGAAP, SEC_EDGAR_ICON).stream().parallel()
+          .map(url -> new ResultItem(this.loadFile(url, String.class, SEC_EDGAR_ICON.equals(url)), SEC_EDGAR_ICON.equals(url)))
+          .filter(item -> !item.icon).findFirst().map(item -> item.content).orElseThrow(() -> new IllegalStateException("No content found"));
         RssDto rssDto = null;		
 		try {
             rssDto = this.xmlMapper.readValue(result, RssDto.class);
@@ -82,24 +87,24 @@ public class NewsFeedConnector implements NewsFeedClient {
 
     @Override
     public byte[] loadCompanyReportZip(String url) {
-        return this.loadFile(url, byte[].class);
+        return this.loadFile(url, byte[].class, false);
     }
 
-    private <T> T loadFile(String url, Class<T> classType) {
-        var result = this.restClient.get().uri(url)
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    private <T> T loadFile(String url, Class<T> classType, Boolean icon) {
+        var result = this.restClient.get().uri(url)                
+                .header("Accept-Encoding", "gzip, deflate")    
+                .header("Host", "www.sec.gov")
+                .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0")
+                .header("Accept", icon ?  "image/avif,image/webp,image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                .header("Accept-Language", "en-US,en;q=0.5")
                 .header("Accept-Encoding", "gzip, deflate")
-                .header("accept-language", "en-US,en;q=0.9")
-                .header("priority", "u=0, i")
-                .header("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36")
-                .header("upgrade-insecure-requests", "1")
-                .header("sec-fetch-user", "?1")
-                .header("sec-fetch-site", "none")
-                .header("sec-fetch-mode", "navigate")
-                .header("sec-fetch-dest", "document")
-                .header("sec-ch-ua-platform", "Linux")
-                .header("sec-ch-ua-mobile", "?0")
-                .header("sec-ch-ua", "Not.A/Brand;v=99", "Chromium;v=136")
+                .header("Connection", "keep-alive")
+                .header(icon ? "Referer" : "Upgrade-Insecure-Requests", icon ? "https://www.sec.gov/Archives/edgar/usgaap.rss.xml" : "1")
+                .header("Sec-Fetch-Dest",  icon ? "image" : "document")
+                .header("Sec-Fetch-Mode", icon ? "no-cors" : "navigate")
+                .header("Sec-Fetch-Site", icon ? "same-origin" : "none")
+                .header(icon ? "TE" : "Sec-Fetch-User", icon ? "trailers" : "?1")
+                .header("Priority", icon ? "u=6" : "u=0, i")
                 .retrieve().body(classType);
         return result;
     }
