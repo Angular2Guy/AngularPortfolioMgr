be9ef861b9042ae708e695151fe91e2b0e95172e
Angular2Guy <angular2guy@gmail.com>
2026-07-17 21:33:03 +0200

    feat: add sec newsfeed

--- backend/src/main/java/ch/xxx/manager/stocks/mapping/open/NewsFeedMapper.java
+++ backend/src/main/java/ch/xxx/manager/stocks/mapping/open/NewsFeedMapper.java
@@ -14,8 +14,11 @@ package ch.xxx.manager.stocks.mapping.open;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import ch.xxx.manager.stocks.entity.CompanyReport;
@@ -24,6 +27,7 @@ import ch.xxx.manager.stocks.entity.dto.RssDto;
 
 @Component
 public class NewsFeedMapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewsFeedMapper.class);
 
     public List<CompanyReportWrapper> convert(RssDto rssDto) {  
       var result =  rssDto.channel().item().stream().filter(myItem ->
@@ -31,13 +35,18 @@ public class NewsFeedMapper {
               || Optional.ofNullable(myItem.xbrlFiling().formType()).stream().anyMatch(value -> value.trim().toUpperCase().contains(CompanyReport.ReportType.QUARTERLY.getReportType()))
       ) .map(myItem -> {
           var period = myItem.xbrlFiling().period();
-          var year = Long.getLong(period.substring(0, 3));
-          var month = Long.getLong(period.substring(4, 5));
-          var day = Long.getLong(period.substring(6, 7));
-          var localDateTime = LocalDateTime.of(year.intValue(), month.intValue(), day.intValue(), 0, 0);
+          LocalDateTime localDateTime = null;
+          try {
+              var year = Integer.parseInt(period.substring(0, 4));
+              var month = Integer.parseInt(period.substring(4, 6));
+              var day = Integer.parseInt(period.substring(6, 8));
+              localDateTime = LocalDateTime.of(year, month, day, 0, 0);
+          }catch (Exception e) {
+              LOGGER.info(period, e);
+          }
           var companyReport = new CompanyReport();
           companyReport.setReportDate(localDateTime);
-          companyReport.setReportType(CompanyReport.ReportType.valueOf(myItem.xbrlFiling().formType().trim()));
+          companyReport.setReportType(Optional.ofNullable(myItem.xbrlFiling().formType()).stream().map(NewsFeedMapper::mapReportType).filter(Objects::nonNull).findFirst().orElseThrow());
           myItem.xbrlFiling().xbrlFiles().stream()
           .filter(xbrlFile -> xbrlFile.type().trim().equalsIgnoreCase(myItem.xbrlFiling().formType().trim()))
           .findFirst().ifPresent(xbrlFile -> {
@@ -45,6 +54,16 @@ public class NewsFeedMapper {
           });          
           return new CompanyReportWrapper(myItem.xbrlFiling().cikNumber().trim(), myItem.enclosure().url(), companyReport);
         }).toList();
+        return result;
+    }
+
+    private static CompanyReport.ReportType mapReportType(String value) {
+        CompanyReport.ReportType result = null;
+        if(value.trim().toUpperCase().contains(CompanyReport.ReportType.QUARTERLY.getReportType())) {
+            result = CompanyReport.ReportType.QUARTERLY;
+        } else if(value.trim().toUpperCase().contains(CompanyReport.ReportType.ANNUAL.getReportType())) {
+            result = CompanyReport.ReportType.ANNUAL;
+        }
         return result;
     }
 }
