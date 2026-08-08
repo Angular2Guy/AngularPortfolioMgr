55aae9655c9cca5cfeb0abbbcafbc0580146f2ac
Angular2Guy <angular2guy@gmail.com>
2026-08-08 08:00:56 +0200

    feat: add component

--- frontend/src/angular/src/app/portfolios/portfolio-overview/components/company-reports/company-reports.component.ts
+++ frontend/src/angular/src/app/portfolios/portfolio-overview/components/company-reports/company-reports.component.ts
@@ -3,23 +3,19 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
+	   http://www.apache.org/licenses/LICENSE-2.0
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { inject, Service } from "@angular/core";
-import { CompanyReport } from "../model/company-report";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Component } from "@angular/core";
 
-@Service()
-export class CompanyReportService {
-    private httpClient = inject(HttpClient);
-
-    public getCompanyReports(): Observable<CompanyReport> {
-        return this.httpClient.get<CompanyReport>('rest/companyreport');
-    }
-}
+@Component({
+  selector: "app-company-reports",
+  imports: [],
+  templateUrl: "./company-reports.component.html",
+  styleUrl: "./company-reports.component.scss",
+})
+export class CompanyReportsComponentComponent {}
